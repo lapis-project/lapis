91714b51425fcd81d0c5e4dcb76c9c9dd3c86e07
kkukelka <17953978+kkukelka@users.noreply.github.com>
2024-06-11 16:24:57 +0200

    feat: add links and theme switcher to header

--- components/app-header.vue
+++ components/app-header.vue
@@ -5,26 +5,29 @@ const t = useTranslations();
 
 const links = computed(() => {
 	return {
-		home: { href: { path: "/" }, label: t("AppHeader.links.home") },
+		articles: { href: { path: "/articles" }, label: t("AppHeader.links.articles") },
+		db: { href: { path: "/db" }, label: t("AppHeader.links.db") },
+		research: { href: { path: "/maps" }, label: t("AppHeader.links.research") },
 	} satisfies Record<string, { href: NavLinkProps["href"]; label: string }>;
 });
 </script>
 
 <template>
 	<header class="border-b">
-		<div class="container flex items-center justify-between gap-4 py-8">
+		<div class="container flex h-16 items-center justify-between gap-4 p-4">
+			<img src="@/assets/lexat.svg" alt="LexAT Logo" class="w-32" />
 			<nav :aria-label="t('AppHeader.navigation-main')">
 				<ul class="flex items-center gap-4" role="list">
 					<li v-for="(link, key) of links" :key="key">
-						<NavLink :href="link.href">
+						<NavLink :href="link.href" class="uppercase">
 							{{ link.label }}
 						</NavLink>
 					</li>
 				</ul>
 			</nav>
 
-			<div class="flex items-center gap-4">
-				<ColorSchemeSwitcher />
+			<div class="relative flex items-center gap-4">
+				<!-- <ColorSchemeSwitcher /> -->
 				<LocaleSwitcher />
 			</div>
 		</div>
--- components/color-scheme-switcher.vue
+++ components/color-scheme-switcher.vue
@@ -1,20 +1,38 @@
 <script lang="ts" setup>
+import { ComputerDesktopIcon, MoonIcon, SunIcon } from "@heroicons/vue/24/outline";
+
 const t = useTranslations();
 
 const colorMode = useColorMode();
 
-const colorSchemes = ["system", "light", "dark"] as const;
+const colorSchemes = [
+	{ name: "system", icon: ComputerDesktopIcon },
+	{ name: "light", icon: SunIcon },
+	{ name: "dark", icon: MoonIcon },
+];
 </script>
 
 <template>
-	<ClientOnly>
-		<label>
-			<span class="sr-only">{{ t("ColorSchemeToggle.change-color-scheme") }}</span>
-			<select v-model="colorMode.preference">
-				<option v-for="colorScheme of colorSchemes" :key="colorScheme" :value="colorScheme">
-					{{ t(`ColorSchemeToggle.color-schemes.${colorScheme}`) }}
-				</option>
-			</select>
-		</label>
-	</ClientOnly>
+	<DropdownMenu>
+		<DropdownMenuTrigger as-child>
+			<Button class="gap-2" size="icon" variant="ghost">
+				<component :is="colorMode.value === 'dark' ? MoonIcon : SunIcon" class="size-6 shrink-0" />
+			</Button>
+		</DropdownMenuTrigger>
+		<DropdownMenuContent>
+			<DropdownMenuItem
+				v-for="colorScheme of colorSchemes"
+				:key="colorScheme.name"
+				class="flex cursor-pointer items-center gap-4"
+				@click="
+					() => {
+						colorMode.preference = colorScheme.name;
+					}
+				"
+			>
+				<component :is="colorScheme.icon" class="size-5" />
+				{{ t(`ColorSchemeToggle.color-schemes.${colorScheme.name}`) }}
+			</DropdownMenuItem>
+		</DropdownMenuContent>
+	</DropdownMenu>
 </template>

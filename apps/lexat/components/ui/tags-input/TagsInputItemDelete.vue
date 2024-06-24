6b531f9f30b60b4ea048935cf4f100619b3cdca9
kkukelka <17953978+kkukelka@users.noreply.github.com>
2024-06-24 10:42:06 +0200

    feat: add tagsinput components

---
+++ apps/lexat/components/ui/tags-input/TagsInputItemDelete.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { XMarkIcon } from "@heroicons/vue/24/solid";
+import { TagsInputItemDelete, type TagsInputItemDeleteProps, useForwardProps } from "radix-vue";
+import { computed, type HTMLAttributes } from "vue";
+
+import { cn } from "@/utils/styles";
+
+const props = defineProps<TagsInputItemDeleteProps & { class?: HTMLAttributes["class"] }>();
+
+const delegatedProps = computed(() => {
+	const { class: _, ...delegated } = props;
+
+	return delegated;
+});
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+	<TagsInputItemDelete
+		v-bind="forwardedProps"
+		:class="cn('flex rounded bg-transparent mr-1', props.class)"
+	>
+		<slot>
+			<XMarkIcon class="size-4" />
+		</slot>
+	</TagsInputItemDelete>
+</template>
---
+++ apps/lexat/components/ui/tags-input/TagsInputItemText.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { TagsInputItemText, type TagsInputItemTextProps, useForwardProps } from "radix-vue";
+import { computed, type HTMLAttributes } from "vue";
+
+import { cn } from "@/utils/styles";
+
+const props = defineProps<TagsInputItemTextProps & { class?: HTMLAttributes["class"] }>();
+
+const delegatedProps = computed(() => {
+	const { class: _, ...delegated } = props;
+
+	return delegated;
+});
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+	<TagsInputItemText
+		v-bind="forwardedProps"
+		:class="cn('py-1 px-2 text-sm rounded bg-transparent', props.class)"
+	/>
+</template>

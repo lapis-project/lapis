d396e8f5cdc3bba02eb30ede47d91075c9ab7894
Jakob Bal <baljakob9@gmail.com>
2024-07-03 12:00:14 +0200

    Feat: Added simple health check for backend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,13 +2,21 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 
 const app = new Hono();
+const healthCheck = new Hono();
 
 app.get("/", (c) => {
 	return c.text("Hello Hono!");
 });
 
+healthCheck.get("/", (c) => {
+	return c.json("OK", 201);
+});
+
+app.route("health", healthCheck);
+
 const port = 3000;
-console.log(`Server is running on port ${port}`);
+// eslint-disable-next-line no-console
+console.log("Server is running on port: %s", port);
 
 serve({
 	fetch: app.fetch,

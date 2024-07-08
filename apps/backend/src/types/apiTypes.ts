c064327faac1cfa612c25f362b06c653c690c9ec
Jakob Bal <baljakob9@gmail.com>
2024-07-08 10:45:11 +0200

    feat: Added first types for the backend

---
+++ apps/backend/src/types/apiTypes.ts
@@ -0,0 +1,51 @@
+interface Category {
+	id: string;
+	name: string;
+}
+
+enum Status {
+	DRAFT,
+	PUBLISHED,
+	ARCHIVED,
+}
+
+enum ResponseInflucenced {
+	F,
+}
+
+interface User {
+	role: string; // Enum
+	username: string;
+	id: string;
+}
+
+interface Article {
+	id: number;
+	user_id: number; // User Object
+	author: User; // User Object
+	category: Category;
+	title: string;
+	content: string;
+	abstract: string;
+	alias: string; // sanitasation of at creation of the article
+	project: string;
+	lang: string;
+	publishedAt: string;
+	updatedAt: string;
+	bibliography: Array<string>;
+	cover: string;
+}
+
+interface ArticleCMS extends Article {
+	status: Status;
+}
+
+interface ArticleTeaser extends Omit<Article, "biblography" | "content"> {}
+
+interface QuestionResponse {
+	responseText: string;
+	timestampAudio: string;
+	responseSelected: number;
+	responseOrder: number;
+	responseInfluenced: ResponseInflucenced;
+}

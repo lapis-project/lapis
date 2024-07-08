31fb204a5007347dd954ae545a5e00f58c122fff
Jakob Bal <baljakob9@gmail.com>
2024-07-08 11:36:55 +0200

    feat: Added valibot types for endpoints

--- apps/backend/src/handler/articleHandler.ts
+++ apps/backend/src/handler/articleHandler.ts
@@ -1,23 +1,28 @@
+import { vValidator } from "@hono/valibot-validator";
 import { Hono } from "hono";
+import { number, object, string } from "valibot";
 
 const articles = new Hono();
 
+const articleRouteSchmema = object({
+	searchTerm: string(),
+	page: number(),
+	offset: number(),
+	pageSize: number(),
+	category: string(), // Does it allow as an enum? ARTICLE | BLOG | NEWS
+});
+
 const articleRoute = articles.get(
 	"/articles/:project",
-	zValidator(
-		"query",
-		z.object({
-			searchTerm: z.string(),
-			page: z.number(),
-			offset: z.number(),
-			pageSize: z.number(),
-			category: z.string(), // Does it allow as an enum? ARTICLE | BLOG | NEWS
-		}),
-	),
-	async (c) => {},
+	vValidator("query", articleRouteSchmema),
+	(c) => {
+		return c.json("OK", 201);
+	},
 );
 
-const articleDetail = articles.get("/article/:alias", async (c) => {});
+const articleDetail = articles.get("/article/:alias", (c) => {
+	return c.json("OK", 201);
+});
 
 export type ArticleRouteType = typeof articleRoute;
 export type ArticleDetailType = typeof articleDetail;
--- apps/backend/src/handler/cmsHandler.ts
+++ apps/backend/src/handler/cmsHandler.ts
@@ -1,46 +1,49 @@
+import { vValidator } from "@hono/valibot-validator";
 import { Hono } from "hono";
+import { array, number, object, optional, string } from "valibot";
 
 const cms = new Hono();
 
-const createNewArticleSchema = z.object({
-	title: z.string(),
-	cover: z.string().optional(), // Wie sind die Bilder abgespeichert? Wo passiert der Optimierungsschritt? Was wird zurückgeliefert?
-	content: z.string(), // Wie wird der Content in verschiedenen Sprachen gespeichert? Innerhalb eines Tupels oder mehrere?
-	category: z.string(), // Does it allow as an enum? ARTICLE | BLOG | NEWS
-	authors: z.array(z.string()),
-	bibliography: z.array(z.string()),
-	publishedAt: z.string(),
-	status: z.string(), // Does it allow as an enum? DRAFT | PUBLISHED | ARCHIVED
-	abstract: z.string(),
-	lang: z.string(), // Does it allow as an enum? EN | FI | SV
+const createNewArticleSchema = object({
+	title: string(),
+	cover: optional(string()), // Wie sind die Bilder abgespeichert? Wo passiert der Optimierungsschritt? Was wird zurückgeliefert?
+	content: string(), // Wie wird der Content in verschiedenen Sprachen gespeichert? Innerhalb eines Tupels oder mehrere?
+	category: string(), // Does it allow as an enum? ARTICLE | BLOG | NEWS
+	authors: array(string()),
+	bibliography: array(string()),
+	publishedAt: string(),
+	status: string(), // Does it allow as an enum? DRAFT | PUBLISHED | ARCHIVED
+	abstract: string(),
+	lang: string(), // Does it allow as an enum? EN | FI | SV
 });
 
-const deleteArticle = cms.delete("/cms/:id", async (c) => {});
+const searchArticleSchema = object({
+	searchTerm: string(),
+	page: number(),
+	offset: number(),
+	pageSize: number(),
+	category: string(), // Does it allow as an enum? ARTICLE | BLOG | NEWS
+});
 
-const editArticle = cms.put("/cms/:id", async (c) => {});
+const deleteArticle = cms.delete("/cms/:id", (c) => {
+	return c.json("OK", 201);
+});
 
-const cmsRoute = cms.get(
-	"/cms/:project",
-	zValidator(
-		"query",
-		z.object({
-			searchTerm: z.string(),
-			page: z.number(),
-			offset: z.number(),
-			pageSize: z.number(),
-			category: z.string(), // Does it allow as an enum? ARTICLE | BLOG | NEWS
-		}),
-	),
-	async (c) => {},
-);
+const editArticle = cms.put("/cms/:id", (c) => {
+	return c.json("OK", 201);
+});
 
-const articleCMSDetail = cms.get("/cms/:id", async (c) => {});
+const cmsRoute = cms.get("/cms/:project", vValidator("query", searchArticleSchema), (c) => {
+	return c.json("OK", 201);
+});
 
-const createNewArticle = cms.post(
-	"/cms",
-	zValidator("json", createNewArticleSchema),
-	async (c) => {},
-);
+const articleCMSDetail = cms.get("/cms/:id", (c) => {
+	return c.json("OK", 201);
+});
+
+const createNewArticle = cms.post("/cms", vValidator("query", createNewArticleSchema), (c) => {
+	return c.json("OK", 201);
+});
 
 export type DeleteArticleType = typeof deleteArticle;
 export type EditArticleType = typeof editArticle;

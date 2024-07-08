ec5a8049fe54e9cd6b96ca59d6b47b8a3704c3be
Jakob Bal <baljakob9@gmail.com>
2024-07-08 10:44:03 +0200

    feat: Added code to connect to db instance

---
+++ apps/backend/src/db/connect.ts
@@ -0,0 +1,21 @@
+import * as dotenv from "dotenv";
+import { Kysely, PostgresDialect } from "kysely";
+import { Pool } from "pg";
+
+import { type DB, Users } from "../types/db";
+
+dotenv.config();
+
+const pool = new Pool({
+	database: process.env.PGDATABASE,
+	host: process.env.PGHOST,
+	port: process.env.PGPORT ? Number(process.env.PGPORT) : 5432,
+	user: process.env.PGUSER,
+	password: process.env.PGPASSWORD,
+});
+
+const dialect = new PostgresDialect({
+	pool,
+});
+
+export const db = new Kysely<DB>({ dialect });

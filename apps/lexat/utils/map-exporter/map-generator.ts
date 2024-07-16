10fdbe313afb3e0ae679cae5aa18cc4113ebc460
kkukelka <17953978+kkukelka@users.noreply.github.com>
2024-07-16 10:06:35 +0200

    feat: roll our own export plugin

---
+++ apps/lexat/utils/map-exporter/map-generator.ts
@@ -0,0 +1,125 @@
+/*
+ * watergis/maplibre-gl-export
+ * https://github.com/watergis/maplibre-gl-export
+ *
+ * I used the source code from the above repository. Thanks so much!
+ *
+ * -----LICENSE------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import "js-loading-overlay";
+
+import { Map as MaplibreMap, type StyleSpecification } from "maplibre-gl";
+
+import {
+	defaultAttributionOptions,
+	defaultMarkerCirclePaint,
+	MapGeneratorBase,
+} from "./map-generator-base";
+import {
+	type DPIType,
+	Format,
+	type FormatType,
+	Size,
+	type SizeType,
+	Unit,
+	type UnitType,
+} from "./types";
+
+export default class MapGenerator extends MapGeneratorBase {
+	/**
+	 * Constructor
+	 * @param map MaplibreMap object
+	 * @param size layout size. default is A4
+	 * @param dpi dpi value. deafult is 300
+	 * @param format image format. default is PNG
+	 * @param unit length unit. default is mm
+	 * @param fileName file name. default is 'map'
+	 */
+	constructor(
+		map: MaplibreMap,
+		size: SizeType = Size.A4,
+		dpi: DPIType = 300,
+		format: FormatType = Format.PNG,
+		unit: UnitType = Unit.mm,
+		fileName = "map",
+		markerCirclePaint = defaultMarkerCirclePaint,
+		attributionOptions = defaultAttributionOptions,
+	) {
+		super(
+			map,
+			size,
+			dpi,
+			format,
+			unit,
+			fileName,
+			"maplibregl-marker",
+			markerCirclePaint,
+			"maplibregl-ctrl-attrib-inner",
+			attributionOptions,
+		);
+	}
+
+	protected getRenderedMap(container: HTMLElement, style: StyleSpecification) {
+		// Render map
+		const renderMap: MaplibreMap = new MaplibreMap({
+			container,
+			style,
+			center: this.map.getCenter(),
+			zoom: this.map.getZoom(),
+			bearing: this.map.getBearing(),
+			pitch: this.map.getPitch(),
+			interactive: false,
+			preserveDrawingBuffer: true,
+			fadeDuration: 0,
+			// attributionControl: false,
+			// hack to read transfrom request callback function
+			transformRequest: this.map._requestManager._transformRequestFn,
+		});
+
+		const terrain = this.map.getTerrain();
+		if (terrain) {
+			// if terrain is enabled, restore pitch correctly
+			renderMap.setMaxPitch(85);
+			renderMap.setPitch(this.map.getPitch());
+		}
+
+		// the below code was added by https://github.com/watergis/maplibre-gl-export/pull/18.
+		const images = this.map.style.imageManager.images;
+		Object.keys(images).forEach((key) => {
+			renderMap.addImage(key, images[key].data);
+		});
+
+		return renderMap;
+	}
+
+	protected override renderMapPost(renderMap: MaplibreMap) {
+		const terrain = this.map.getTerrain();
+		if (terrain) {
+			// if terrain is enabled, set terrain for rendered map object
+			renderMap.setTerrain({
+				source: terrain.source,
+				exaggeration: terrain.exaggeration,
+			});
+		}
+
+		return renderMap;
+	}
+}
---
+++ apps/lexat/utils/map-exporter/types.ts
@@ -0,0 +1,127 @@
+/*
+ * watergis/maplibre-gl-export
+ * https://github.com/watergis/maplibre-gl-export
+ *
+ * I used the source code from the above repository. Thanks so much!
+ *
+ * -----LICENSE------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+export interface AttributionStyle {
+	textSize: number;
+	textHaloColor: string;
+	textHaloWidth: number;
+	textColor: string;
+	fallbackTextFont: Array<string>;
+}
+
+export interface AttributionOptions {
+	style?: AttributionStyle;
+	visibility?: "none" | "visible";
+	// TODO: top-left and bottom-left have issues of text-max-width setting
+	position?: "bottom-right" | "top-right";
+}
+
+export interface CirclePaint {
+	"circle-radius"?: number;
+	"circle-color"?: string;
+	"circle-blur"?: number;
+	"circle-opacity"?: number;
+	"circle-translate"?: Array<number>;
+	"circle-translate-anchor"?: "map" | "viewport";
+	"circle-stroke-width": number;
+	"circle-stroke-color": string;
+}
+
+export interface ControlOptions {
+	PageSize?: SizeType;
+	PageOrientation?: PageOrientationType;
+	Format?: FormatType;
+	DPI?: number;
+	Crosshair?: boolean;
+	PrintableArea?: boolean;
+	AllowedSizes?: Array<
+		"A2" | "A3" | "A4" | "A5" | "A6" | "B2" | "B3" | "B4" | "B5" | "B6" | "LETTER"
+	>;
+	Filename?: string;
+	markerCirclePaint?: CirclePaint;
+	attributionOptions?: AttributionOptions;
+}
+
+export const DPI = {
+	72: 72,
+	96: 96,
+	200: 200,
+	300: 300,
+	400: 400,
+} as const;
+export type DPIType = (typeof DPI)[keyof typeof DPI];
+
+export const Format = {
+	JPEG: "jpg",
+	PNG: "png",
+	PDF: "pdf",
+	SVG: "svg",
+} as const;
+export type FormatType = (typeof Format)[keyof typeof Format];
+
+export const PageOrientation = {
+	Landscape: "landscape",
+	Portrait: "portrait",
+} as const;
+export type PageOrientationType = (typeof PageOrientation)[keyof typeof PageOrientation];
+
+export const Size = {
+	// A0, A1, B0, B1 are not working well.
+	// A0: [1189, 841],
+	// A1: [841, 594],
+	LETTER: [279, 216], // 8.5x11 - works
+	//TABLOID: [432,279] // 11x17 - not working currently prints to 11.68x8.27 in landscape
+	A2: [594, 420],
+	A3: [420, 297],
+	A4: [297, 210],
+	A5: [210, 148],
+	A6: [148, 105],
+	// B0: [1414, 1000],
+	// B1: [1000, 707],
+	B2: [707, 500],
+	B3: [500, 353],
+	B4: [353, 250],
+	B5: [250, 176],
+	B6: [176, 125],
+} as const;
+export type SizeType = (typeof Size)[keyof typeof Size];
+
+export interface Translation {
+	PageSize: string;
+	PageOrientation: string;
+	Format: string;
+	DPI: string;
+	Generate: string;
+	LanguageName: string;
+	LanguageCode: string;
+}
+
+export const Unit = {
+	// don't use inch unit. because page size setting is using mm unit.
+	in: "in",
+	mm: "mm",
+} as const;
+export type UnitType = (typeof Unit)[keyof typeof Unit];

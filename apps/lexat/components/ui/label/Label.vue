90853977c7c90b2417f9478efe1d538db9ce8cac
kkukelka <17953978+kkukelka@users.noreply.github.com>
2024-07-29 16:35:25 +0200

    feat: add input and label component

---
+++ apps/lexat/components/ui/label/Label.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { Label, type LabelProps } from "radix-vue";
+import { computed, type HTMLAttributes } from "vue";
+
+import { cn } from "@/utils/styles";
+
+const props = defineProps<LabelProps & { class?: HTMLAttributes["class"] }>();
+
+const delegatedProps = computed(() => {
+	const { class: _, ...delegated } = props;
+
+	return delegated;
+});
+</script>
+
+<template>
+	<!-- eslint-disable-next-line vuejs-accessibility/label-has-for -->
+	<Label
+		v-bind="delegatedProps"
+		:class="
+			cn(
+				'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+				props.class,
+			)
+		"
+	>
+		<slot />
+	</Label>
+</template>

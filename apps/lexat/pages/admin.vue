737f2fa4b05829294962f3e59fd3461536deaf5c
kkukelka <17953978+kkukelka@users.noreply.github.com>
2024-08-05 16:05:32 +0200

    feat: improve cms layout

--- apps/lexat/pages/admin.vue
+++ apps/lexat/pages/admin.vue
@@ -1,7 +1,23 @@
 <script lang="ts" setup>
+import { useToast } from "@/components/ui/toast/use-toast";
+
+const { toast } = useToast();
+
 const t = useTranslations();
 
 const content = ref<string>("<p>Hello Tiptap</p>");
+
+const title = ref<string>("");
+
+const abstract = ref<string>("");
+
+const saveArticle = () => {
+	// do something
+	toast({
+		title: "Article Saved",
+		description: "Your changes have successfully been saved.",
+	});
+};
 
 const collections = [
 	{
@@ -36,22 +52,44 @@ usePageMetadata({
 					<li v-for="collection in collections" :key="collection.alias">{{ collection.name }}</li>
 				</ul>
 			</aside>
-			<div class="col-span-3 rounded border p-8">
-				<div class="mb-6 grid w-full max-w-sm items-center gap-1.5">
-					<Label for="title">{{ t("AdminPage.editor.title") }}</Label>
-					<Input id="title" type="text" :placeholder="t('AdminPage.editor.title')" />
+			<div class="col-span-4 rounded border p-8">
+				<div class="mb-8 flex justify-between border-b pb-8">
+					<div>
+						<h3 class="text-3xl font-semibold">Untitled</h3>
+						<p class="text-foreground/70">ID: 1231231</p>
+					</div>
+					<div>
+						<Button @click="saveArticle">Save</Button>
+					</div>
 				</div>
-				<div class="mb-6 grid w-full max-w-sm items-center gap-1.5">
-					<Label for="abstract">{{ t("AdminPage.editor.abstract") }}</Label>
-					<Input id="abstract" type="text" :placeholder="t('AdminPage.editor.abstract')" />
-				</div>
-				<div class="grid w-full items-center gap-1.5">
-					<Label for="content">{{ t("AdminPage.editor.content") }}</Label>
-					<ClientOnly>
-						<TextEditor v-model="content" class="w-full" />
-					</ClientOnly>
+				<div class="bg-background">
+					<div class="mb-6 grid w-full max-w-sm items-center gap-1.5">
+						<Label for="title">{{ t("AdminPage.editor.title") }}</Label>
+						<Input
+							id="title"
+							v-model="title"
+							type="text"
+							:placeholder="t('AdminPage.editor.title')"
+						/>
+					</div>
+					<div class="mb-6 grid w-full max-w-sm items-center gap-1.5">
+						<Label for="abstract">{{ t("AdminPage.editor.abstract") }}</Label>
+						<Textarea
+							id="abstract"
+							v-model="abstract"
+							type="text"
+							:placeholder="t('AdminPage.editor.abstract')"
+						/>
+					</div>
+					<div class="grid w-full items-center gap-1.5">
+						<Label for="content">{{ t("AdminPage.editor.content") }}</Label>
+						<ClientOnly>
+							<TextEditor v-model="content" class="w-full" />
+						</ClientOnly>
+					</div>
 				</div>
 			</div>
 		</div>
+		<Toaster />
 	</MainContent>
 </template>

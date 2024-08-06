b0c411ef007355748ddf91d83f80851ef2617e18
Jakob Bal <baljakob9@gmail.com>
2024-08-06 14:32:10 +0200

    feat: added middleware for logging and prettyJSON

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,23 +1,35 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { prettyJSON } from "hono/pretty-json";
 
 import articles from "./handler/articleHandler";
 import cms from "./handler/cmsHandler";
 import questions from "./handler/questionHandler";
 
 const app = new Hono();
+
+app.use(logger());
+app.use(prettyJSON());
+
+// Healthcheck for the k8s server
 const healthCheck = new Hono();
 
 const hc = healthCheck.get("/", (c) => {
 	return c.json("OK", 201);
 });
 
+app.notFound((c) => c.json({ message: "Not Found", ok: false }, 404));
+
+// Including all new routes
 app.route("/health", healthCheck);
 app.route("/articles", articles);
 app.route("/questions", questions);
 app.route("/cms", cms);
 
 export type HealthType = typeof hc;
+
+// Starting the server
 const port = Number(process.env.PORT) || 3000;
 // eslint-disable-next-line no-console
 console.log("Server is running on port: %s", port);

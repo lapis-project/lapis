43ed9a0689cbe6c2ab535e110f821d8f586a9fb9
Jakob Bal <baljakob9@gmail.com>
2024-08-24 15:48:56 +0000

    chore: merge Feat/backend db into main branch (#28)
    * chore: Added env.local file to gitignore
    
    * feat: added example file for env file
    
    * feat: added command to generate types from DB
    
    * feat: generated types of DB
    
    * feat: Added types into the creation of DBpool
    
    * feat: added middleware for logging and prettyJSON
    
    * feat: Added watch mode when starting server
    
    * chore: i cannot read
    
    * feat: added script to start dev mode from root folder
    
    * feat: Added repository file for db queries for question handler
    
    * feat: Added code for the question endpoint
    
    * chore: added .env
    
    * chore: added .dockerignore for files
    
    * feat: Created Dockerfile for Backend
    
    * feat: Adapted settings to exclude search results from lockfile
    
    * chore: Removed code from Dockerfile which builds Backend
    
    * chore: Renamed Dockerfile to Dockerfile_Frontend
    
    * feat: Created a matrix for the deployment of frontend and backend images
    
    * fix: Fixed error where the permissions for the matrix were missing
    
    * feat: added vitest to backend project
    
    * Adapted handlers
    
    * Added env vars for build of Backend
    
    * fix: Changed install option to not use frozen lockfile
    
    * Chore: formatting
    
    * chore: Updated dependencies
    
    * fix: Reinstalled backend packages
    
    * feat: Set up basic vitest env

---
+++ apps/backend/vitest.config.ts
@@ -0,0 +1,29 @@
+import { configDefaults, defineConfig } from "vitest/config";
+
+export default defineConfig({
+	test: {
+		globals: true,
+		include: ["**/tests/**/(*.)+(spec|test).+(ts|tsx|js)"],
+		exclude: [...configDefaults.exclude, "**/sandbox/**", "**/*.case.test.+(ts|tsx|js)"],
+		setupFiles: ["./.vitest.config/setup-vitest.ts"],
+		coverage: {
+			enabled: true,
+			provider: "v8",
+			reportsDirectory: "./coverage/raw/default",
+			reporter: ["json", "text", "html"],
+			exclude: [
+				...(configDefaults.coverage.exclude ?? []),
+				"benchmarks",
+				"runtime_tests",
+				"build.ts",
+				"src/test-utils",
+
+				// types are compile-time only, so their coverage cannot be measured
+				"src/**/types.ts",
+				"src/jsx/intrinsic-elements.ts",
+				"src/utils/http-status.ts",
+			],
+		},
+		pool: "forks",
+	},
+});

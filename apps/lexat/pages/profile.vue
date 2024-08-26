cf79f2d0736e8b83a457afdb85e936e3aee8385c
kkukelka <17953978+kkukelka@users.noreply.github.com>
2024-08-26 16:58:13 +0200

    feat: add dummy profile page

---
+++ apps/lexat/pages/profile.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const t = useTranslations();
+
+usePageMetadata({
+	title: t("ProfilePage.meta.title"),
+});
+</script>
+
+<template>
+	<MainContent class="container grid content-start gap-y-8 py-8">
+		<PageTitle>{{ t("ProfilePage.title") }}</PageTitle>
+	</MainContent>
+</template>

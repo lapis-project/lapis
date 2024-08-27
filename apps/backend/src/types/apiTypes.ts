b433b9effb51a37418194fe07a51cf321ed82e45
Jakob Bal <baljakob9@gmail.com>
2024-08-27 16:37:51 +0200

    refactor: added export statements to apiTypes

--- apps/backend/src/types/apiTypes.ts
+++ apps/backend/src/types/apiTypes.ts
@@ -1,25 +1,25 @@
-interface Category {
+export interface Category {
 	id: string;
 	name: string;
 }
 
-enum Status {
+export enum Status {
 	DRAFT,
 	PUBLISHED,
 	ARCHIVED,
 }
 
-enum ResponseInflucenced {
+export enum ResponseInflucenced {
 	F,
 }
 
-interface User {
+export interface User {
 	role: string; // Enum
 	username: string;
 	id: string;
 }
 
-interface Article {
+export interface Article {
 	id: number;
 	user_id: number; // User Object
 	author: User; // User Object
@@ -36,16 +36,13 @@ interface Article {
 	cover: string;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-interface ArticleCMS extends Article {
+export interface ArticleCMS extends Article {
 	status: Status;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-interface ArticleTeaser extends Omit<Article, "biblography" | "content"> {}
+export interface ArticleTeaser extends Omit<Article, "biblography" | "content"> {}
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-interface QuestionResponse {
+export interface QuestionResponse {
 	responseText: string;
 	timestampAudio: string;
 	responseSelected: number;

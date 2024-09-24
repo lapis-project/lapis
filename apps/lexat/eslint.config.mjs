d8297b30f5ff8c46c0a77343f0973d6b5f2d890d
kkukelka <17953978+kkukelka@users.noreply.github.com>
2024-09-24 10:30:25 +0200

    refactor: eslint config into flat config

---
+++ apps/lexat/eslint.config.mjs
@@ -0,0 +1,12 @@
+export default [
+	{
+		extends: [
+			"@acdh-oeaw/eslint-config",
+			"@acdh-oeaw/eslint-config/strict",
+			"@acdh-oeaw/eslint-config-vue",
+			"@acdh-oeaw/eslint-config-nuxt",
+			"@acdh-oeaw/eslint-config-playwright",
+			"plugin:eslint-plugin-tailwindcss/recommended",
+		],
+	},
+];

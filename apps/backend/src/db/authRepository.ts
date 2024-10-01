4690672fe9e7bc235d9045729b49b668203ec182
Jakob Bal <baljakob9@gmail.com>
2024-10-01 16:36:06 +0200

    feat: extended requests for getting and creating a user

--- apps/backend/src/db/authRepository.ts
+++ apps/backend/src/db/authRepository.ts
@@ -1,17 +1,49 @@
+import type { Userroles } from "@/types/db";
+
 import { db } from "./connect";
 
 export async function getUser(username: string) {
 	return await db
 		.selectFrom("user_account")
+		.innerJoin("user_roles", "user_account.role_id", "user_roles.id")
 		.where("username", "=", username)
-		.select(["username", "password", "id"])
+		.select([
+			"username",
+			"password",
+			"user_account.id",
+			"firstname",
+			"lastname",
+			"user_roles.role_name",
+			"email",
+		])
 		.executeTakeFirst();
 }
 
-export async function createUser(username: string, password: string) {
+export async function createUser(
+	username: string,
+	password: string,
+	email: string,
+	user_role: Userroles,
+	firstname: string,
+	lastname: string | undefined,
+) {
 	return await db
 		.insertInto("user_account")
-		.values({ username, password })
+		.columns(["role_id", "username", "password", "email", "firstname", "lastname"])
+		.expression((exp) =>
+			exp
+				.selectFrom("user_roles")
+				.select((eb) => [
+					"id as role_id",
+					eb.val(username).as("username"),
+					eb.val(password).as("password"),
+					eb.val(email).as("email"),
+					eb.val(firstname).as("firstname"),
+					eb.val(lastname).as("lastname"),
+				])
+				.where("role_name", "=", user_role),
+		)
+		// .values({ username, password, email, user_role, firstname, lastname })
 		.returning(["id"])
 		.executeTakeFirst();
 }

fe656b18363600baf22f74f2893e2892f79f8e65
kkukelka <17953978+kkukelka@users.noreply.github.com>
2024-10-01 14:38:12 +0200

    chore: remove mockup geojson data

--- apps/lexat/nuxt.config.ts
+++ apps/lexat/nuxt.config.ts
@@ -72,6 +72,7 @@ export default defineNuxtConfig({
 		compressPublicAssets: true,
 		prerender: {
 			routes: ["/manifest.webmanifest", "/robots.txt", "/sitemap.xml"],
+			concurrency: 1,
 		},
 	},
 	postcss: {

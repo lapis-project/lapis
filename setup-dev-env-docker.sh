96954194bb194f6206c4a5f10c71bd7aa37f504c
jaketpr-b <baljakob9@gmail.com>
2024-10-07 14:36:14 +0000

    build: improve local development setup (#46)
    * fix: Fixed an issue where the db would be unreachable for new users
    
    * chore: Added sql create script for the database with data
    
    * feat: Added healthchecks to the containers and made the setup automatic for datainsertion
    
    * chore(lexat): add dev-local script
    
    * Refactor: only contains db now
    
    * refactor: renamed file
    
    * refactor: changed return object to be empty instead of null
    
    * refactor: removed unnecessary code
    
    * Added env variable for frontend
    
    * chore: formatting
    
    * chore: replaced dump
    
    ---------
    
    Co-authored-by: kkukelka <17953978+kkukelka@users.noreply.github.com>

---
+++ setup-dev-env-docker.sh
@@ -0,0 +1,30 @@
+# setup-dev-env.sh
+#!/bin/bash
+
+# This script sets a development environment for the lapis project with functioning database
+# and backend server based on the pulled code
+
+# Check if pnpm exists on the machine
+if ! [ -x "$(command -v pnpm)" ]; then
+		echo "pnpm could not be found, please install pnpm before running this script"
+		echo "for example with corepack enable or npm install -g pnpm"
+		exit 1
+fi
+
+# Install the dependencies
+pnpm install
+
+# Set the environment variables
+set -a && . ./.env.localsetup && set +a
+# Build the docker images and run the containers
+docker build -t lapis-dev-backend:latest -f ./Dockerfile_Backend .
+docker build -t lapis-dev-frontend:latest -f ./Dockerfile_Frontend .
+docker compose --env-file .env.localsetup up -d --wait
+
+# Copy the data data into the database from the dump
+docker exec -i lapis-dev-database-1 psql -U $PGUSER -p $PGPORT $PGDATABASE  < ./db/lapis_dump_011024.sql
+
+# docker compose --env-file .env.localsetup down
+
+# Run the script to build the types
+# pnpm run start:devSetup

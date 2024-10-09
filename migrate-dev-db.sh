38cb1b1e30927270342db6b7877a70f2747e7b39
Jakob Bal <baljakob9@gmail.com>
2024-10-09 13:22:36 +0200

    fix: replaced pg_restore with psql command for migrations

--- migrate-dev-db.sh
+++ migrate-dev-db.sh
@@ -3,5 +3,5 @@
 
 docker exec -i oeaw-postgres-2 pg_dump -U lapis_dev lapis_dev -t user_account > user_account_dump.dump
 docker exec -i oeaw-postgres-2 psql -U lapis_dev lapis_dev < ./db/lapis_dump.sql
-docker exec -i oeaw-postgres-2 pg_restore -U lapis_dev -d lapis_dev -t user_account ./user_account_dump.dump
+docker exec -i oeaw-postgres-2 psql -U lapis_dev lapis_dev < ./user_account_dump.dump
 rm user_account_dump.dump

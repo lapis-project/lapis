f927114ba649c9ac0a7d1f0ab85354099cf1ddf9
kkukelka <17953978+kkukelka@users.noreply.github.com>
2024-10-15 15:21:06 +0200

    feat(lexat): use email instead of username for auth

--- apps/lexat/components/login-form.vue
+++ apps/lexat/components/login-form.vue
@@ -1,21 +1,12 @@
 <script setup lang="ts">
 import { toTypedSchema } from "@vee-validate/zod";
 import { useForm } from "vee-validate";
-import { computed } from "vue";
 import * as z from "zod";
 
 import { Button } from "@/components/ui/button";
 import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useToast } from "@/components/ui/toast/use-toast";
-
-export interface Props {
-	isSignUp?: boolean;
-}
-
-const props = withDefaults(defineProps<Props>(), {
-	isSignUp: false,
-});
 
 const localePath = useLocalePath();
 
@@ -25,54 +16,37 @@ const { toast } = useToast();
 
 const t = useTranslations();
 
+const user = useUser();
+
 const signInSchema = toTypedSchema(
 	z.object({
-		username: z.string(),
-		password: z.string(),
+		email: z.string({ required_error: "Please specify an e-mail address" }),
+		password: z.string({ required_error: "Please specify a password" }),
 	}),
 );
 
-const signUpSchema = toTypedSchema(
-	z
-		.object({
-			username: z.string().min(2, t("Auth.username_min_length")),
-			password: z.string().min(8, t("Auth.password_min_length")),
-			confirmPassword: z.string().min(8, t("Auth.password_min_length")),
-			// email: z.string().email(t("Auth.email_invalid")), // currently not implemented
-		})
-		.refine((data) => data.password === data.confirmPassword, {
-			message: t("Auth.password_mismatch"),
-			path: ["confirmPassword"],
-		}),
-);
-
-const formSchema = computed(() => {
-	return props.isSignUp ? signUpSchema : signInSchema;
-});
-
 const { handleSubmit } = useForm({
-	validationSchema: formSchema,
+	validationSchema: signInSchema,
 });
 
 const onSubmit = handleSubmit(async (formValues) => {
 	try {
-		const apiUrl = props.isSignUp ? "/auth/signup" : "/auth/login"; // Toggle API endpoint
+		const apiUrl = "/auth/login";
 
 		const response = await $fetch(apiUrl, {
 			baseURL: env.public.apiBaseUrl,
 			method: "POST",
-			body: {
-				...formValues,
-			},
+			body: { ...formValues },
 			credentials: "include",
 		});
 		if (response) {
+			user.value = response;
 			await navigateTo(localePath("/admin/articles"));
 		}
 	} catch (error) {
 		toast({
-			title: props.isSignUp ? t("Auth.signup_failed") : t("Auth.login_failed"),
-			description: error || (props.isSignUp ? t("Auth.signup_failed") : t("Auth.login_failed")),
+			title: t("Auth.login_failed"),
+			description: error || t("Auth.login_failed"),
 			variant: "destructive",
 		});
 	}
@@ -81,15 +55,17 @@ const onSubmit = handleSubmit(async (formValues) => {
 
 <template>
 	<form class="w-2/3 space-y-6" @submit="onSubmit">
-		<FormField v-slot="{ componentField }" name="username">
+		<FormField v-slot="{ componentField }" name="email">
 			<FormItem>
-				<FormLabel>{{ t("Auth.username") }}</FormLabel>
+				<FormLabel>{{ t("Auth.email") }}</FormLabel>
 				<FormControl>
-					<Label class="sr-only" for="username">{{ t("Auth.username") }}</Label>
+					<Label class="sr-only" for="email">{{ t("Auth.email") }}</Label>
 					<Input
-						id="username"
-						:placeholder="t('Auth.username')"
-						type="text"
+						id="email"
+						autocomplete="username"
+						:placeholder="t('Auth.email')"
+						required
+						type="email"
 						v-bind="componentField"
 					/>
 				</FormControl>
@@ -104,6 +80,7 @@ const onSubmit = handleSubmit(async (formValues) => {
 					<Label class="sr-only" for="password">{{ t("Auth.password") }}</Label>
 					<Input
 						id="password"
+						autocomplete="current-password"
 						:placeholder="t('Auth.password')"
 						type="password"
 						v-bind="componentField"
@@ -113,39 +90,7 @@ const onSubmit = handleSubmit(async (formValues) => {
 			</FormItem>
 		</FormField>
 
-		<!-- Only show confirmPassword and email for sign-up -->
-		<div v-if="props.isSignUp">
-			<FormField v-slot="{ componentField }" name="confirmPassword">
-				<FormItem>
-					<FormLabel>{{ t("Auth.confirmPassword") }}</FormLabel>
-					<FormControl>
-						<Input
-							id="confirmPassword"
-							:placeholder="t('Auth.confirmPasswordPlaceholder')"
-							type="password"
-							v-bind="componentField"
-						/>
-					</FormControl>
-					<FormMessage />
-				</FormItem>
-			</FormField>
-			<!--
-				<FormField v-slot="{ componentField }" name="email">
-					<FormItem>
-						<FormLabel>{{ t("Auth.email") }}</FormLabel>
-						<FormControl>
-							<Input
-								id="email"
-								type="email"
-								:placeholder="t('Auth.email')"
-								v-bind="componentField"
-							/>
-						</FormControl>
-						<FormMessage />
-					</FormItem>
-				</FormField> -->
-		</div>
-		<Button type="submit"> {{ props.isSignUp ? t("Auth.signup") : t("Auth.login") }} </Button>
+		<Button type="submit"> {{ t("Auth.login") }} </Button>
 		<Toaster />
 	</form>
 </template>

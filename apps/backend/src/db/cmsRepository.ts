e01a4034f83f3a333fb18ba0cb1e076ee1ad70d7
Jakob Bal <baljakob9@gmail.com>
2024-10-15 09:44:23 +0200

    feat: wrote endpoints for cms to fetch all meta information

--- apps/backend/src/db/cmsRepository.ts
+++ apps/backend/src/db/cmsRepository.ts
@@ -23,3 +23,35 @@ export async function getAllPhenomenon(projectId: string) {
 }
 
 // export async function get
+export async function getAllUserPhenKat(project_id: string) {
+	const selectAllPhaen = db
+		.selectFrom("phenomenon")
+		.select(({ eb }) => [
+			eb.val("phenomenon.id").as("id"),
+			eb.val("phenomenon.phenomenon_name").as("name"),
+			eb.val("phenomenon").as("category"),
+		]);
+	const selectAllKat = db
+		.selectFrom("post_type")
+		.select(({ eb }) => [
+			eb.val("post_type.id").as("id"),
+			eb.val("post_type.name").as("name"),
+			eb.val("category").as("category"),
+		]);
+
+	const selectAllEditors = db
+		.selectFrom("user_account")
+		.select(({ eb }) => [
+			eb.val("user_account.id").as("id"),
+			eb.val("user_account.firstname || user_account.lastname").as("name"),
+			eb.val("user").as("category"),
+		])
+		.innerJoin("user_has_role", "user_account.id", "user_has_role.user_id")
+		.innerJoin("user_roles", "user_has_role.role_id", "user_roles.id")
+		.where("user_roles.role_name", "=", "editor");
+	const projectIdParsed = parseInt(project_id);
+	if (Number.isNaN(projectIdParsed) || projectIdParsed < 0) {
+		return await selectAllPhaen.unionAll(selectAllKat).unionAll(selectAllEditors).execute();
+	}
+	return await selectAllPhaen.unionAll(selectAllKat).unionAll(selectAllEditors).execute();
+}

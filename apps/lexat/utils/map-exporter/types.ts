cc9a99a6fe42368e06900aefbf7009771b1c4b77
Kilian Kukelka <17953978+kkukelka@users.noreply.github.com>
2024-11-12 14:01:17 +0100

    feat: add option to hide legend in map export (#61)
    * chore: footer logos
    
    * feat: add option to hide legend in map export

--- apps/lexat/utils/map-exporter/types.ts
+++ apps/lexat/utils/map-exporter/types.ts
@@ -61,6 +61,7 @@ export interface ControlOptions {
 		"A2" | "A3" | "A4" | "A5" | "A6" | "B2" | "B3" | "B4" | "B5" | "B6" | "LETTER"
 	>;
 	Filename?: string;
+	IncludeLegend?: boolean;
 	markerCirclePaint?: CirclePaint;
 	attributionOptions?: AttributionOptions;
 }

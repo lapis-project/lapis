bb774fd4709fd0955748c4fbdb6b62c9910d48c1
Jakob Bal <baljakob9@gmail.com>
2024-11-19 00:03:26 +0100

    feat: wrote article endpoints

---
+++ apps/backend/src/db/articleRepository.ts
@@ -0,0 +1,173 @@
+import { sql } from "kysely";
+import { jsonBuildObject } from "kysely/helpers/postgres";
+
+import { db } from "@/db/connect";
+import { jsonbBuildObject } from "@/lib/dbHelper";
+import type { Poststatus } from "@/types/db";
+
+export async function getArticleByAlias(alias: string) {
+	const query = db
+		.with("authors", (query) =>
+			query
+				.selectFrom("user_post")
+				.innerJoin("user_account", "user_post.user_id", "user_account.id")
+				.innerJoin("post", "user_post.post_id", "post.id")
+				.where("post.alias", "=", alias)
+				.where("user_post.user_id", "is not", null)
+				.select(["user_account.firstname", "user_account.lastname", "user_post.post_id"]),
+		)
+		.with("bibliography_query", (query) =>
+			query
+				.selectFrom("bibliography_post")
+				.innerJoin("bibliography", "bibliography_post.bibliography_id", "bibliography.id")
+				.innerJoin("post", "bibliography_post.post_id", "post.id")
+				.where("post.alias", "=", alias)
+				.select(["bibliography.name_bibliography", "bibliography_post.post_id"]),
+		)
+		.with("phenomenon_query", (query) =>
+			query
+				.selectFrom("phenomenon_post")
+				.innerJoin("phenomenon", "phenomenon_post.phenomenon_id", "phenomenon.id")
+				.innerJoin("post", "phenomenon_post.post_id", "post.id")
+				.where("post.alias", "=", alias)
+				.select(["phenomenon.id", "phenomenon.phenomenon_name", "phenomenon_post.post_id"]),
+		)
+		.selectFrom("post")
+		.innerJoin("post_type", "post.post_type_id", "post_type.id")
+		.innerJoin("user_account", "user_account.id", "post.creator_id")
+		.leftJoin("bibliography_query", "bibliography_query.post_id", "post.id")
+		.leftJoin("authors", "authors.post_id", "post.id")
+		.leftJoin("phenomenon_query", "phenomenon_query.post_id", "post.id")
+		.where("post.alias", "=", alias)
+		.select(({ eb }) => [
+			"post.id as post_id",
+			"post.title",
+			"post.alias",
+			"post.cover",
+			"post.cover_alt",
+			"post.abstract",
+			"post.content",
+			"post.post_status",
+			"post.lang",
+			"post.published_at",
+			"post.updated_at",
+			"post.created_at",
+			"post.citation",
+			"user_account.firstname as creator_firstname",
+			"user_account.lastname as creator_lastname",
+			"post_type.post_type_name",
+			eb.fn
+				.coalesce(
+					eb.fn
+						.jsonAgg(
+							jsonBuildObject({
+								phenomenon_id: eb.ref("phenomenon_query.id"),
+								name: eb.ref("phenomenon_query.phenomenon_name"),
+							}),
+						)
+						.filterWhere("phenomenon_query.post_id", "is not", null),
+					sql`'[]'`,
+				)
+				.as("phenomenon"),
+			eb.fn
+				.coalesce(
+					eb.fn
+						.jsonAgg(
+							jsonbBuildObject({
+								name: eb.ref("bibliography_query.name_bibliography"),
+							}),
+						)
+						.filterWhere("bibliography_query.post_id", "is not", null),
+					sql`'[]'`,
+				)
+				.as("bibliography"),
+			eb.fn
+				.coalesce(
+					eb.fn
+						.jsonAgg(
+							jsonBuildObject({
+								firstname: eb.ref("authors.firstname"),
+								lastname: eb.ref("authors.lastname"),
+							}),
+						)
+						.filterWhere("authors.post_id", "is not", null),
+					sql`'[]'`,
+				)
+				.as("authors"),
+		])
+		.groupBy(["post.id", "post_type.post_type_name", "user_account.id"]);
+	return await query.executeTakeFirst();
+}
+
+export async function getAllArticlesByProject(
+	projectId: number,
+	pageSize: number,
+	offset: number,
+	searchTerm: string,
+	postType: string,
+	postStatus: Poststatus,
+) {
+	const query = db
+		.with("post_query", (query) =>
+			query
+				.selectFrom("post")
+				.innerJoin("project_post", "post.id", "project_post.post_id")
+				.leftJoin("user_post", "post.id", "user_post.post_id")
+				.leftJoin("user_account", (join) =>
+					join
+						.onRef("user_post.user_id", "=", "user_account.id")
+						.onRef("post.creator_id", "=", "user_account.id"),
+				)
+				.innerJoin("post_type", "post.post_type_id", "post_type.id")
+				.where("project_post.project_id", "=", projectId)
+				.where("post.title", "~*", searchTerm)
+				.where("post_type.post_type_name", "~*", postType)
+				.where("post.post_status", "=", postStatus)
+				.select(({ eb }) => [
+					sql<number>`ROW_NUMBER() OVER (order by post.id) as rn`,
+					eb.ref("post.id").as("post_id"),
+					eb.ref("post.title").as("title"),
+					eb.ref("post.alias").as("alias"),
+					eb.ref("post.content").as("content"),
+					eb.ref("post.abstract").as("abstract"),
+					eb.ref("post.post_status").as("status"),
+					eb.ref("post.cover").as("cover"),
+					eb.ref("post.cover_alt").as("cover_alt"),
+					eb.ref("post_type.post_type_name").as("post_type"),
+					eb.ref("post.creator_id").as("creator_id"),
+					eb.fn
+						.jsonAgg(
+							jsonbBuildObject({
+								firstname: eb.ref("user_account.firstname"),
+								lastname: eb.ref("user_account.lastname"),
+							}),
+						)
+						.as("authors"),
+				])
+				.groupBy(["post.id", "post_type.post_type_name"]),
+		)
+		.selectFrom("post_query")
+		//.select(({ eb, fn }) => [fn.jsonAgg(eb.ref("post_query")).as("articles")])
+		.select(({ eb, fn }) => [
+			fn
+				.jsonAgg(
+					jsonBuildObject({
+						post_id: eb.ref("post_query.post_id"),
+						title: eb.ref("post_query.title"),
+						alias: eb.ref("post_query.alias"),
+						content: eb.ref("post_query.content"),
+						abstract: eb.ref("post_query.abstract"),
+						status: eb.ref("post_query.status"),
+						post_type: eb.ref("post_query.post_type"),
+						authors: eb.ref("authors"),
+						cover: eb.ref("cover"),
+						cover_alt: eb.ref("cover_alt"),
+					}),
+				)
+				.filterWhere("rn", ">", offset)
+				.filterWhere("rn", "<=", pageSize + offset)
+				.as("articles"),
+			fn.countAll().as("total"),
+		]);
+	return await query.execute();
+}
--- apps/backend/src/handler/articleHandler.ts
+++ apps/backend/src/handler/articleHandler.ts
@@ -1,27 +1,96 @@
-import { vValidator } from "@hono/valibot-validator";
 import { Hono } from "hono";
-import { number, object, string } from "valibot";
+import { number, optional, safeParse, string } from "valibot";
+
+import { getAllArticlesByProject, getArticleByAlias } from "@/db/articleRepository";
 
 const articles = new Hono();
 
+/*
+* Currently commented out
+* Maybe needed a bit later?
 const articleRouteSchmema = object({
-	searchTerm: string(),
-	page: number(),
-	offset: number(),
-	pageSize: number(),
 	category: string(), // Does it allow as an enum? ARTICLE | BLOG | NEWS
 });
+*/
 
-const articleRoute = articles.get(
-	"/articles/:project",
-	vValidator("json", articleRouteSchmema),
-	(c) => {
-		return c.json("OK", 201);
-	},
-);
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const articleRoute = articles.get("/articles/:project", async (c) => {
+	const projectId = c.req.param("project");
 
-const articleDetail = articles.get("/detail/:alias", (c) => {
-	return c.json("OK", 201);
+	if (!projectId || Number.isNaN(Number(projectId))) {
+		return c.json("Provided projectId is not a number", 400);
+	}
+
+	const { searchTerm, page, offset, pageSize, category } = c.req.query();
+
+	// TODO Refactor this functions into utilities
+	const stringSchema = optional(string());
+	const numberSchema = optional(number());
+
+	const pageSizeParsed = Number(pageSize ?? 20);
+	const pageNumParsed = Number(page ?? 1);
+
+	const offsetParsed = Number(offset ?? 0);
+	if (!safeParse(stringSchema, searchTerm).success) {
+		return c.json("Provided search term is not a string", 400);
+	}
+
+	if (!safeParse(numberSchema, pageNumParsed).success) {
+		return c.json("Provided page number is not a number", 400);
+	}
+
+	if (!safeParse(numberSchema, offsetParsed).success) {
+		return c.json("Provided offset number is not a number", 400);
+	}
+
+	if (!safeParse(numberSchema, pageSizeParsed).success) {
+		return c.json("Provided pagesize number is not a number", 400);
+	}
+
+	const queryOffset = (pageNumParsed - 1) * pageSizeParsed + offsetParsed;
+
+	const fetchedArticles = await getAllArticlesByProject(
+		Number(projectId),
+		pageSizeParsed,
+		queryOffset,
+		searchTerm ?? "",
+		category ?? "",
+		"Published",
+	);
+
+	const articles = fetchedArticles[0]?.articles;
+	const totalCount = Number(fetchedArticles[0]?.total);
+	const requestUrl = c.req.url;
+	return c.json(
+		{
+			prev:
+				pageNumParsed > 1 && totalCount !== 0 && !(queryOffset > totalCount)
+					? requestUrl.replace(`page=${String(pageNumParsed)}`, `page=${String(pageNumParsed - 1)}`)
+					: null,
+			next:
+				totalCount > pageSizeParsed + queryOffset
+					? requestUrl.replace(`page=${String(pageNumParsed)}`, `page=${String(pageNumParsed + 1)}`)
+					: null,
+			articles: articles ? articles : [],
+			currentPage: requestUrl,
+			totalResults: totalCount,
+		},
+		201,
+	);
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const articleDetail = articles.get("/detail/:alias", async (c) => {
+	const articleAlias = c.req.param("alias");
+	const aliasSchema = string();
+	const result = safeParse(aliasSchema, articleAlias);
+
+	if (!result.success) {
+		return c.json("Provided alias is not a string", 400);
+	}
+
+	const fetchedArticle = await getArticleByAlias(articleAlias);
+	return c.json({ article: fetchedArticle }, 201);
 });
 
 export type ArticleRouteType = typeof articleRoute;

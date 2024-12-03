480900555a11c92af756ff4407c15b9c097a5a40
Jakob Bal <baljakob9@gmail.com>
2024-12-03 11:05:08 +0100

    docs: wrote documentation for the authhandlers

--- apps/backend/src/handler/authHandler.ts
+++ apps/backend/src/handler/authHandler.ts
@@ -42,8 +42,17 @@ const signupSchema = object({
 	lastname: optional(pipe(string(), trim(), minLength(1))),
 });
 
+// Checks if the user is privileged for admin or higher
+// If not will return 403
 auth.use("/signup", checkIfPrivilegedForAdminOrHigher);
 
+/**
+ * Gets the current session of the user
+ * If the user is logged in it will return the user object with a status code of 200
+ * If the user is not logged in it will return null with a status code of 401
+ * @returns status code 200 with the user object if the user is logged in
+ * @returns status code 401 if the user is not logged in
+ */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const getSession = auth.get("/session", async (c) => {
 	const session = c.get("session");
@@ -54,6 +63,13 @@ const getSession = auth.get("/session", async (c) => {
 	return c.json(null, 401);
 });
 
+/**
+ * Logs the user with the provided credentials, which are provided in the body, in.
+ * Returns the user object with a status code of 200 if the login was successful
+ * Returns a status code of 401 if the login was unsuccessful
+ * @returns status code 200 with the user object if the login was successful
+ * @returns status code 401 if the login was unsuccessful
+ */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const login = auth.post("/login", vValidator("json", loginSchema), async (c, next) => {
 	const { email, password } = c.req.valid("json");
@@ -84,6 +100,13 @@ const login = auth.post("/login", vValidator("json", loginSchema), async (c, nex
 	return c.json(userObject, 200);
 });
 
+/**
+ * Logs the user out by invalidating the session and setting the session cookie to an empty string
+ * Returns a status code of 201 if the logout was successful
+ * Returns a status code of 401 if the user is not logged in
+ * @returns status code 201 if the logout was successful
+ * @returns status code 401 if the user is not logged in
+ */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const logoutUser = auth.post("/logout", async (c) => {
 	const session = c.get("session");
@@ -95,6 +118,15 @@ const logoutUser = auth.post("/logout", async (c) => {
 	return c.json("OK", 201);
 });
 
+/**
+ * Creates a new user account in the database with the provided credentials. E-Mail adress must be unique and from oeaw or univie.
+ * If the signup was successful the user is logged in and a session cookie is set.
+ * Returns the user object with a status code of 200 if the signup was successful
+ * Returns a status code of 409 if the user already exists
+ * Returns a status code of 400 if the email is not from oeaw or univie
+ * @returns status code 200 with the user object if the signup was successful
+ * @returns status code 409 if the user already exists
+ */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const signupUser = auth.post("/signup", vValidator("json", signupSchema), async (c) => {
 	const { username, password, email, user_role, firstname, lastname } = c.req.valid("json");

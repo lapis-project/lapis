4af0402e4bc2991146af055a3db28866eee7c261
Jakob Bal <baljakob9@gmail.com>
2024-12-03 10:59:48 +0100

    feat: set up basic migration for db

---
+++ apps/backend/kysely.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "kysely-ctl";
+
+import { db } from "./src/db/connect";
+
+export default defineConfig({
+	kysely: db,
+	migrations: {
+		allowJS: true,
+		migrationFolder: "./../../db/migrations",
+	},
+	// replace me with a real dialect instance OR a dialect name + `dialectConfig` prop
+	//   migrations: {
+	//     migrationFolder: "migrations",
+	//   },
+	//   plugins: [],
+	//   seeds: {
+	//     seedFolder: "seeds",
+	//   }
+});

4953ebc6105367621ee16cd6a34990be291e6090
Kilian Kukelka <17953978+kkukelka@users.noreply.github.com>
2025-01-20 09:41:51 +0100

    feat: redirect after login (#83)

--- apps/lexat/middleware/protected.ts
+++ apps/lexat/middleware/protected.ts
@@ -1,8 +1,10 @@
 export default defineNuxtRouteMiddleware(async () => {
 	const localePath = useLocalePath();
+	const route = useRoute();
 
 	const user = useUser();
+	const redirectPath = route.path;
 	if (!user.value) {
-		return navigateTo(localePath("/login"));
+		return navigateTo(localePath(`/login?redirect=${redirectPath}`));
 	}
 });

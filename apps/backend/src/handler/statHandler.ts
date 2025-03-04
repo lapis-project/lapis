13feb9a6c3d087fb03a50e690406ed1022e208a8
Jakob Bal <baljakob9@gmail.com>
2025-03-04 15:24:15 +0100

    feat: added handler for statistical data about the project

--- apps/backend/src/handler/statHandler.ts
+++ apps/backend/src/handler/statHandler.ts
@@ -1,18 +1,28 @@
 import { Hono } from "hono";
 
-import { getAllStatData } from "@/db/statRepository";
+import { getAgeBuckets, getAllStatData } from "@/db/statRepository";
 import type { Context } from "@/lib/context";
 
 const statistics = new Hono<Context>().get("/", async (c) => {
-	const { projectId } = c.req.query();
+	const { projectId, buckets } = c.req.query();
 
 	let projectIdParsed = parseInt(projectId ?? "1");
 	if (Number.isNaN(projectIdParsed) || projectIdParsed < 0) {
 		projectIdParsed = 1;
 	}
 
+	// Check if buckets is an array of numbers
+	// If not set default buckets
+	let parsedBuckets = JSON.parse(buckets ?? "[]") as Array<number>;
+	if (parsedBuckets.length === 0) {
+		parsedBuckets = [30, 50, 100];
+	}
+
 	const data = await getAllStatData(projectIdParsed);
-	return c.json(data, 200);
+
+	const ageBuckets = await getAgeBuckets(projectIdParsed, parsedBuckets);
+
+	return c.json({ age: ageBuckets[0], ...data[0] }, 200);
 });
 
 export default statistics;

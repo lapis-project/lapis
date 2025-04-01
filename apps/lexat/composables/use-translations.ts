d394f2dbb5c912335547aec139ce444c66ef1331
Kilian Kukelka <17953978+kkukelka@users.noreply.github.com>
2025-04-01 13:13:36 +0200

    fix: use localization for register options (#110)

--- apps/lexat/composables/use-translations.ts
+++ apps/lexat/composables/use-translations.ts
@@ -1,5 +1,7 @@
 import type { Locale, Schema } from "@/config/i18n.config";
 
+export type TranslateFn = ReturnType<typeof useTranslations>;
+
 export function useTranslations() {
 	const { t } = useI18n<Schema, Locale>();
 

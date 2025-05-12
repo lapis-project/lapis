9724f4cfd3bc4c9e37e6345ba3b1fde32b15f106
kkukelka <17953978+kkukelka@users.noreply.github.com>
2025-05-12 13:00:49 +0200

    chore: add alert component

---
+++ apps/lexat/components/ui/alert/Alert.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue";
+
+import { cn } from "@/utils/styles";
+
+import { type AlertVariants, alertVariants } from ".";
+
+const props = defineProps<{
+	class?: HTMLAttributes["class"];
+	variant?: AlertVariants["variant"];
+}>();
+</script>
+
+<template>
+	<div :class="cn(alertVariants({ variant }), props.class)" data-slot="alert" role="alert">
+		<slot />
+	</div>
+</template>
---
+++ apps/lexat/components/ui/alert/AlertTitle.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue";
+
+import { cn } from "@/utils/styles";
+
+const props = defineProps<{
+	class?: HTMLAttributes["class"];
+}>();
+</script>
+
+<template>
+	<div
+		:class="cn('col-start-2 line-clamp-1 min-h-4 font-medium tracking-tight', props.class)"
+		data-slot="alert-title"
+	>
+		<slot />
+	</div>
+</template>

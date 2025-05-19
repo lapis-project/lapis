2f0c77e648222598b92ff5bd15cd818793cbfb6e
kkukelka <17953978+kkukelka@users.noreply.github.com>
2025-05-19 18:29:23 +0200

    feat: add tabs components

---
+++ apps/lexat/components/ui/tabs/TabsContent.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { reactiveOmit } from "@vueuse/core";
+import { TabsContent, type TabsContentProps } from "reka-ui";
+import type { HTMLAttributes } from "vue";
+
+import { cn } from "@/utils/styles";
+
+const props = defineProps<TabsContentProps & { class?: HTMLAttributes["class"] }>();
+
+const delegatedProps = reactiveOmit(props, "class");
+</script>
+
+<template>
+	<TabsContent
+		:class="cn('flex-1 outline-none', props.class)"
+		data-slot="tabs-content"
+		v-bind="delegatedProps"
+	>
+		<slot />
+	</TabsContent>
+</template>

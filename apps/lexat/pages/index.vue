0a4e70b2256c46d46c6b4c35a05d043adc821cd8
kkukelka <17953978+kkukelka@users.noreply.github.com>
2025-06-11 10:33:55 +0200

    fix: project citation wording

--- apps/lexat/pages/index.vue
+++ apps/lexat/pages/index.vue
@@ -108,12 +108,12 @@ usePageMetadata({
 				<h2 class="text-3xl font-semibold">{{ t("HomePage.citation.title") }}</h2>
 				<p>{{ t("HomePage.citation.text") }}:</p>
 				<blockquote>
-					LexAT21: Atlas zur lexikalischen Variation in Österreich im 21. Jahrhundert. Herausgegeben
-					von Alexandra N. Lenz. Konzipiert und entwickelt von Markus Pluschkovits, Jakob Bal,
-					Kilian Kukelka, Anja Wittibschlager und Daniel Schopper. Unter Mitarbeit von Amelie Dorn,
-					Jan Höll, Katharina Korecky-Kröll, Wolfgang Koppensteiner, Claudia Mattes, Markus
-					Pluschkovits, Rita Stiglbauer, Florian David Tavernier, Anja Wittibschlager, Theresa
-					Ziegler, Kerstin Lorenz und Eric Schirl.
+					LexAT21 - Atlas zur lexikalischen Variation in Österreich im 21. Jahrhundert.
+					Herausgegeben von Alexandra N. Lenz. Konzipiert und entwickelt von Jakob Bal, Kilian
+					Kukelka, Markus Pluschkovits, Daniel Schopper und Anja Wittibschlager. Unter Mitarbeit von
+					Amelie Dorn, Jan Höll, Katharina Korecky-Kröll, Wolfgang Koppensteiner, Claudia Mattes,
+					Markus Pluschkovits, Rita Stiglbauer, Florian David Tavernier, Anja Wittibschlager,
+					Theresa Ziegler, Kerstin Lorenz und Eric Schirl.
 				</blockquote>
 			</div>
 		</section>

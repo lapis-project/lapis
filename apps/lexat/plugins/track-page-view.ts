29d1045cd65d88665197766c15fb26746b42f392
Kilian Kukelka <17953978+kkukelka@users.noreply.github.com>
2025-07-02 14:53:07 +0200

    fix(lexat): track pageview on first page load (#157)
    * fix(lexat): track pageview on first page load
    
    * fix: add meaninful referrer for initial page load

--- apps/lexat/plugins/track-page-view.ts
+++ apps/lexat/plugins/track-page-view.ts
@@ -1,10 +1,50 @@
-export default defineNuxtPlugin(() => {
-	const router = useRouter();
+import type { RouteLocationNormalized } from "vue-router";
 
-	router.afterEach((to, from) => {
-		// don't trigger on query param changes
-		if (to.path !== from.path) {
-			trackPageView(to, from);
-		}
-	});
+export default defineNuxtPlugin((nuxtApp) => {
+	const router = useRouter();
+	const route = useRoute();
+
+	// TODO find solution using SSR request headers
+	// if (import.meta.server) {
+	// 	const event = nuxtApp.ssrContext?.event;
+	// 	const referer = event?.node.req.headers.referer ?? "/";
+	// 	const host = event?.node.req.headers.host ?? "lexat21.lapis-online.at";
+	// 	const rawProtocol = event?.node.req.headers["x-forwarded-proto"];
+	// 	const protocol = (Array.isArray(rawProtocol) ? rawProtocol[0] : rawProtocol) ?? "https";
+
+	// 	let refPath = "/";
+	// 	try {
+	// 		refPath = new URL(referer, `${protocol}://${host}`).pathname;
+	// 	} catch (e) {
+	// 		console.error(e);
+	// 		refPath = "/";
+	// 	}
+
+	// 	const fakeFromRoute = {
+	// 		...route,
+	// 		fullPath: refPath,
+	// 	} as RouteLocationNormalized;
+
+	// 	trackPageView(route, fakeFromRoute);
+	// }
+
+	if (import.meta.client) {
+		// track on initial load
+		nuxtApp.hook("app:mounted", () => {
+			const fakeFromRoute = {
+				...route,
+				fullPath: new URL(document.referrer || "/", window.location.origin).pathname,
+			} as RouteLocationNormalized;
+
+			trackPageView(route, fakeFromRoute);
+		});
+
+		// track on client-side route changes
+		router.afterEach((to, from) => {
+			// don't trigger on query param changes
+			if (to.path !== from.path) {
+				trackPageView(to, from);
+			}
+		});
+	}
 });

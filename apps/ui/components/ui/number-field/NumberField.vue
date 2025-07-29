337975e2b8d2b2490abad8f25120bda6e42b0e5a
kkukelka <17953978+kkukelka@users.noreply.github.com>
2025-07-29 16:54:07 +0200

    chore: add number field components

---
+++ apps/ui/components/ui/number-field/NumberField.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { reactiveOmit } from "@vueuse/core";
+import {
+	NumberFieldRoot,
+	type NumberFieldRootEmits,
+	type NumberFieldRootProps,
+	useForwardPropsEmits,
+} from "reka-ui";
+import type { HTMLAttributes } from "vue";
+
+import { cn } from "@/utils/styles";
+
+const props = defineProps<NumberFieldRootProps & { class?: HTMLAttributes["class"] }>();
+const emits = defineEmits<NumberFieldRootEmits>();
+
+const delegatedProps = reactiveOmit(props, "class");
+
+const forwarded = useForwardPropsEmits(delegatedProps, emits);
+</script>
+
+<template>
+	<NumberFieldRoot v-bind="forwarded" :class="cn('grid gap-1.5', props.class)">
+		<slot />
+	</NumberFieldRoot>
+</template>

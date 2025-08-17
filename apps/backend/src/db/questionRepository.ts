503908c40364b9e5f050722342c537a2a8e7fa3b
Jakob Bal <baljakob9@gmail.com>
2025-08-17 15:11:11 +0200

    feat(backend): added new endpoint for checking of surveys in the database

--- apps/backend/src/db/questionRepository.ts
+++ apps/backend/src/db/questionRepository.ts
@@ -357,3 +357,18 @@ export async function getResultsByPhaen(
 
 	return await query.execute();
 }
+
+export async function getAllSurveys() {
+	return await db
+		.selectFrom("survey")
+		.innerJoin("survey_conducted", "survey.id", "survey_conducted.survey_id")
+		.select(() => [
+			"survey.id",
+			"survey.survey_name",
+			"survey.description",
+			// workaround to return the count as number instead of string
+			sql<number>`CAST(COUNT(survey_conducted.id) AS INTEGER)`.as("conducted_num"),
+		])
+		.groupBy("survey.id")
+		.execute();
+}

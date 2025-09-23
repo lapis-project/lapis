cdeac2f61d402ee7252dbb9b7d0406e220f5fea7
kkukelka <17953978+kkukelka@users.noreply.github.com>
2025-09-23 14:52:22 +0200

    fix(lexat): use new shadcn-vue component names

--- apps/lexat/components/page-pagination.vue
+++ apps/lexat/components/page-pagination.vue
@@ -29,18 +29,18 @@ const onUpdatePage = (newPage: number) => {
 		:total="totalItems"
 		@update:page="onUpdatePage"
 	>
-		<PaginationList v-slot="{ items }" class="flex items-center justify-center gap-1">
-			<PaginationPrev />
+		<PaginationContent v-slot="{ items }" class="flex items-center justify-center gap-1">
+			<PaginationPrevious />
 			<template v-for="(item, index) in items">
-				<PaginationListItem v-if="item.type === 'page'" :key="index" as-child :value="item.value">
+				<PaginationItem v-if="item.type === 'page'" :key="index" as-child :value="item.value">
 					<Button class="size-10 p-0" :variant="item.value === page ? 'default' : 'outline'">
 						{{ item.value }}
 					</Button>
-				</PaginationListItem>
+				</PaginationItem>
 				<PaginationEllipsis v-else :key="item.type" :index="index" />
 			</template>
 
 			<PaginationNext />
-		</PaginationList>
+		</PaginationContent>
 	</Pagination>
 </template>

36c5ce64f3b1f5c0b912b709a7d6fffbdbcb58b7
kkukelka <17953978+kkukelka@users.noreply.github.com>
2025-11-18 13:33:29 +0100

    chore(ui): migrate app to nuxt 4

---
+++ apps/ui/app/components/ui/sheet/SheetTitle.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { reactiveOmit } from "@vueuse/core";
+import { DialogTitle, type DialogTitleProps } from "reka-ui";
+import type { HTMLAttributes } from "vue";
+
+import { cn } from "@/utils/styles";
+
+const props = defineProps<DialogTitleProps & { class?: HTMLAttributes["class"] }>();
+
+const delegatedProps = reactiveOmit(props, "class");
+</script>
+
+<template>
+	<DialogTitle
+		:class="cn('text-foreground font-semibold', props.class)"
+		data-slot="sheet-title"
+		v-bind="delegatedProps"
+	>
+		<slot />
+	</DialogTitle>
+</template>

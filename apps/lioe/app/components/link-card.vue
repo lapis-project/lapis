6fbcf3bfc50b3a765e44804f858e1102dcb4e97f
meeriemarie <cc231054@ustp-students.at>
2026-01-29 09:53:03 +0100

    feat(lioe): adding card component and translations

---
+++ apps/lioe/app/components/link-card.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+const props = defineProps<{
+	title: string;
+	description: string;
+	content: string;
+	action: string;
+}>();
+
+const t = useTranslations();
+</script>
+
+<template>
+	<Card>
+		<CardHeader>
+			<CardTitle>{{ t(props.title) }}</CardTitle>
+			<CardDescription>{{ t(props.description) }}</CardDescription>
+		</CardHeader>
+		<CardContent>{{ t(props.content) }}</CardContent>
+		<CardFooter>
+			<CardAction>
+				<Button id="follow-link" variant="outline">{{ t(props.action) }}</Button>
+			</CardAction>
+		</CardFooter>
+	</Card>
+</template>

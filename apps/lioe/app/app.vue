1afdb737ef74e9993d57d5e7593ba64d34c28d25
Kilian Kukelka <17953978+kkukelka@users.noreply.github.com>
2026-02-03 15:48:46 +0100

    feat(lioe): reimplement locale switcher with nuxt/ui (#214)

--- apps/lioe/app/app.vue
+++ apps/lioe/app/app.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+import * as locales from "@nuxt/ui/locale";
+const { locale } = useI18n();
+</script>
+
 <template>
 	<NuxtRouteAnnouncer />
-	<UApp>
+	<UApp :locale="locales[locale]">
 		<NuxtLayout>
 			<NuxtPage />
 			<NuxtLoadingIndicator />

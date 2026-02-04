cf60ea31e127554122ea0f1897706daa3dad5e7f
Marie Rohler <146174200+meeriemarie@users.noreply.github.com>
2026-02-04 11:54:38 +0100

    feat(lioe): add landing page and rework header (#215)
    * feat(lioe): adding card component and translations
    
    * fix(lioe): switching link card to nuxtui
    
    * fix(lioe): modify app-header to use nuxtui components

---
+++ apps/lioe/app/components/link-card.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+const props = defineProps<{
+	title: string;
+	description: string;
+	content: string;
+	action: string;
+	link: string;
+}>();
+
+const t = useTranslations();
+</script>
+
+<template>
+	<UCard class="flex items-center flex-col" variant="outline">
+		<template #header>
+			<h3 class="text-lg font-medium">{{ t(props.title) }}</h3>
+			<p class="text-sm h-10">{{ t(props.description) }}</p>
+		</template>
+
+		<p class="text-sm">{{ t(props.content) }}</p>
+
+		<template #footer>
+			<UButton id="follow-link" target="_blank" :to="t(props.link)" variant="outline">
+				{{ t(props.action) }}
+			</UButton>
+		</template>
+	</UCard>
+</template>

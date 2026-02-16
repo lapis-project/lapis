15bea74375867fcd0999da0cde7e0381f73e5808
Marie Rohler <146174200+meeriemarie@users.noreply.github.com>
2026-02-16 12:19:41 +0100

    feat(lioe): use nuxt UI components (#220)
    * feat(lioe): re-implementing header using nuxt ui components
    
    * fix(lioe): changing footer to nuxtui component

--- apps/lioe/app/components/link-card.vue
+++ apps/lioe/app/components/link-card.vue
@@ -14,10 +14,10 @@ const t = useTranslations();
 	<UCard class="flex items-center flex-col" variant="outline">
 		<template #header>
 			<h3 class="text-lg font-medium">{{ t(props.title) }}</h3>
-			<p class="text-sm h-10">{{ t(props.description) }}</p>
+			<p class="text-sm h-14">{{ t(props.description) }}</p>
 		</template>
 
-		<p class="text-sm">{{ t(props.content) }}</p>
+		<p class="text-sm h-28">{{ t(props.content) }}</p>
 
 		<template #footer>
 			<UButton id="follow-link" target="_blank" :to="t(props.link)" variant="outline">

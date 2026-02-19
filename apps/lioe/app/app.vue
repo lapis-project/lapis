cb92d7d9b1b650c93ab85cfdd6422ac3f9500bc0
Kilian Kukelka <17953978+kkukelka@users.noreply.github.com>
2026-02-19 12:47:30 +0100

    refactor(lioe): cleanup html structure (#225)

--- apps/lioe/app/app.vue
+++ apps/lioe/app/app.vue
@@ -2,14 +2,18 @@
 import * as locales from "@nuxt/ui/locale";
 
 const { locale } = useI18n();
+const t = useTranslations();
 </script>
 
 <template>
 	<NuxtRouteAnnouncer />
 	<UApp :locale="locales[locale]">
-		<NuxtLayout>
+		<SkipLink target-id="main-content">{{ t("DefaultLayout.skip-to-main-content") }}</SkipLink>
+		<NuxtLoadingIndicator />
+		<AppHeader class="fixed top-0 left-0 right-0" />
+		<UMain id="main-content" :tabindex="-1">
 			<NuxtPage />
-			<NuxtLoadingIndicator />
-		</NuxtLayout>
+		</UMain>
+		<AppFooter />
 	</UApp>
 </template>

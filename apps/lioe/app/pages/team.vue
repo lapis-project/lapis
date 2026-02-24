2084df49039bffff3b5e10df850e5ab11e969cac
meeriemarie <cc231054@ustp-students.at>
2026-02-24 12:29:53 +0100

    fix(lioe): new styling for team member cards

--- apps/lioe/app/pages/team.vue
+++ apps/lioe/app/pages/team.vue
@@ -2,6 +2,7 @@
 const members = ref([
 	{
 		name: "Alexandra N. Lenz",
+		description: "Institute Director",
 		avatar: {
 			src: "https://www.oeaw.ac.at/fileadmin/_processed_/b/6/csm_team_lenz_2025_5264433d37.png",
 			alt: "Alexandra Lenz",
@@ -9,6 +10,7 @@ const members = ref([
 	},
 	{
 		name: "Jakob Bal",
+		description: "Research Software Engineer",
 		avatar: {
 			src: "",
 			alt: "Jakob Bal",
@@ -16,6 +18,7 @@ const members = ref([
 	},
 	{
 		name: "Wolfgang Koppensteiner",
+		description: "Academy Scientist",
 		avatar: {
 			src: "",
 			alt: "Wolfgang Koppensteiner",
@@ -23,6 +26,7 @@ const members = ref([
 	},
 	{
 		name: "Kilian Kukelka",
+		description: "Research Software Engineer",
 		avatar: {
 			src: "https://www.oeaw.ac.at/fileadmin/_processed_/2/1/csm_team_kukelka_82c9a0bb19.png",
 			alt: "Kilian Kukelka",
@@ -30,6 +34,7 @@ const members = ref([
 	},
 	{
 		name: "Markus Pluschkovits",
+		description: "Project Management and Consulting",
 		avatar: {
 			src: "https://www.oeaw.ac.at/fileadmin/_processed_/d/d/csm_team_pluschkovits_a90fbb9922.png",
 			alt: "Markus Pluschkovits",
@@ -37,6 +42,7 @@ const members = ref([
 	},
 	{
 		name: "Olivia Reichl",
+		description: "Research Software Engineer",
 		avatar: {
 			src: "https://www.oeaw.ac.at/fileadmin/_processed_/0/2/csm_team_reichl_42d4d05d72.png",
 			alt: "Olivia Reichl",
@@ -44,6 +50,7 @@ const members = ref([
 	},
 	{
 		name: "Marie Rohler",
+		description: "Research Software Assistant",
 		avatar: {
 			src: "https://www.oeaw.ac.at/fileadmin/_processed_/e/8/csm_team_rohler_52bd732012.png",
 			alt: "Marie Rohler",
@@ -51,6 +58,7 @@ const members = ref([
 	},
 	{
 		name: "Daniel Schopper",
+		description: "Co-Head DHRI",
 		avatar: {
 			src: "https://www.oeaw.ac.at/fileadmin/_processed_/8/0/csm_team_schopper_af4a51f2a8.png",
 			alt: "Daniel Schopper",
@@ -58,6 +66,7 @@ const members = ref([
 	},
 	{
 		name: "Philipp Stöckle",
+		description: "Group Leader DynAT",
 		avatar: {
 			src: "https://www.oeaw.ac.at/fileadmin/_processed_/8/c/csm_team_stoeckle_1b71b0b8b2.png",
 			alt: "Philipp Stöckle",
@@ -65,6 +74,7 @@ const members = ref([
 	},
 	{
 		name: "Katharina Wünsche",
+		description: "Research Software Engineer",
 		avatar: {
 			src: "https://www.oeaw.ac.at/fileadmin/_processed_/2/7/csm_team_wuensche_97ff68f5a4.png",
 			alt: "Katharina Wünsche",
@@ -72,6 +82,7 @@ const members = ref([
 	},
 	{
 		name: "Anja Wittibschlager",
+		description: "",
 		avatar: {
 			src: "",
 			alt: "Anja Wittibschlager",
@@ -93,13 +104,27 @@ const members = ref([
 			>
 			</UPageHeader>
 
-			<UPageList>
-				<UPageCard v-for="(member, index) in members" :key="index" variant="ghost">
+			<UPageGrid class="mt-8">
+				<UPageCard
+					v-for="(member, index) in members"
+					:key="index"
+					:ui="{
+						wrapper: 'flex flex-col flex-1 items-start sm:items-center sm:align-center h-32',
+						body: 'p-4',
+					}"
+					variant="outline"
+				>
 					<template #body>
-						<UUser :avatar="member.avatar" :name="member.name" size="3xl"></UUser>
+						<UUser
+							:avatar="member.avatar"
+							:description="member.description"
+							:name="member.name"
+							orientation="horizontal"
+							size="2xl"
+						></UUser>
 					</template>
 				</UPageCard>
-			</UPageList>
+			</UPageGrid>
 		</div>
 	</UPage>
 </template>

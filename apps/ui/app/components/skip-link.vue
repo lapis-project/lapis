949657d4cbeb42e6766b85cbb56b4a2bc580a369
kkukelka <17953978+kkukelka@users.noreply.github.com>
2026-03-03 12:19:00 +0100

    chore(ui): move components into app folder and remove shadcn-sepcific classes

---
+++ apps/ui/app/components/skip-link.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const { targetId } = defineProps<{
+	targetId: string;
+}>();
+
+const route = useRoute();
+
+/** @see {@link https://bugzilla.mozilla.org/show_bug.cgi?id=308064} */
+function onClick() {
+	const element = document.getElementById(targetId);
+	element?.focus();
+}
+</script>
+
+<template>
+	<NuxtLink
+		class="fixed -translate-y-full rounded-sm bg-neutral-950 px-4 py-3 text-white transition focus:translate-y-0 dark:bg-white dark:text-neutral-950"
+		:href="{ hash: `#${targetId}`, query: route.query }"
+		@click="onClick"
+	>
+		<slot />
+	</NuxtLink>
+</template>

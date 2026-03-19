4113c6633ca00714ea1f2cd2c5ef8ad7ef7ce471
meeriemarie <cc231054@ustp-students.at>
2026-03-19 13:46:34 +0100

    feat(lioe): defining new collection for demo page

--- apps/lioe/content.config.ts
+++ apps/lioe/content.config.ts
@@ -4,7 +4,7 @@ export default defineContentConfig({
 	collections: {
 		content_en: defineCollection({
 			type: "page",
-			source: "en/**/*.md",
+			source: "en/**/index.md",
 			// This schema creates the "form" you see in Nuxt Studio
 			schema: z.object({
 				title: z.string(),
@@ -14,11 +14,40 @@ export default defineContentConfig({
 		}),
 		content_de: defineCollection({
 			type: "page",
-			source: "de/**/*.md",
+			source: "de/**/index.md",
 			schema: z.object({
 				title: z.string(),
 				description: z.string(),
 				linksTitle: z.string(),
+			}),
+		}),
+		content_demo_de: defineCollection({
+			type: "page",
+			source: "de/**/wboedemo.md",
+			schema: z.object({
+				title: z.string(),
+				description: z.string(),
+				navHome: z.string(),
+				navArticles: z.string(),
+				navDatabase: z.string(),
+				navMapping: z.string(),
+				navInformation: z.string(),
+				articlesTitle: z.string(),
+				articlesText: z.string(),
+				fundamentalTitle: z.string(),
+				fundamentalText: z.string(),
+				fullTitle: z.string(),
+				fullText: z.string(),
+				retrogradeTitle: z.string(),
+				retrogradeText: z.string(),
+				databaseTitle: z.string(),
+				databaseText: z.string(),
+				mappingTitle: z.string(),
+				mappingText: z.string(),
+				infoTitle: z.string(),
+				infoText: z.string(),
+				faqTitle: z.string(),
+				faqText: z.string(),
 			}),
 		}),
 	},

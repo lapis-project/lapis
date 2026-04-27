553ca692bae38a2640e7d1a2955452d555dc0e4a
meeriemarie <cc231054@ustp-students.at>
2026-04-27 18:03:42 +0200

    feat(wboe): finishing info card component and rendering on German page

--- apps/wboe/app/pages/resources.vue
+++ apps/wboe/app/pages/resources.vue
@@ -1,5 +1,40 @@
+<script setup lang="ts">
+import type { Collections } from "@nuxt/content";
+
+const { locale } = useI18n();
+
+const { data: info } = await useAsyncData(
+	`resources`,
+	async () => {
+		const collectionName = `resources_${locale.value}` as keyof Collections;
+		const result = await queryCollection(collectionName).first();
+
+		return result;
+	},
+	{
+		watch: [locale],
+	},
+);
+</script>
+
 <template>
-	<div>
-		<h1>Informationen</h1>
-	</div>
+	<UPage
+		v-if="info"
+		:ui="{
+			root: 'pt-6 pb-12 lg:pt-10 lg:pb-18 container',
+		}"
+	>
+		<div class="max-w-4xl mx-auto">
+			<UPageHeader :title="info.title" />
+
+			<div class="my-6 text-center">
+				<p>{{ info.description }}</p>
+			</div>
+
+			<ContentRenderer
+				class="grid grid-cols-1 md:grid-cols-2 gap-8 justify-items-center"
+				:value="info"
+			/>
+		</div>
+	</UPage>
 </template>
---
+++ apps/wboe/content/en/resources.md
@@ -0,0 +1,4 @@
+---
+title: Information about WBO
+description: Auf dieser Seite finden Sie weiterführende Informationen und Zusatzmaterialien zum WBÖ.
+---

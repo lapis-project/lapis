064b71b5967af60061f1ca89edafdbd06b2b72cd
kkukelka <17953978+kkukelka@users.noreply.github.com>
2026-06-17 13:38:31 +0200

    fix(lexat): wait for page to be loaded instead of using timeout

--- apps/lexat/app/router.options.ts
+++ apps/lexat/app/router.options.ts
@@ -2,14 +2,23 @@ import type { RouterConfig } from "@nuxt/schema";
 
 export default {
 	scrollBehavior(to, from, savedPosition) {
+		const nuxtApp = useNuxtApp();
+
 		if (savedPosition) {
 			return savedPosition;
 		}
+		if (to.hash) {
+			return { el: to.hash, behavior: "smooth" };
+		}
+		if (to.path === from.path) {
+			return;
+		}
 
+		// wait for the next page to finish rendering
 		return new Promise((resolve) => {
-			setTimeout(() => {
+			nuxtApp.hook("page:finish", () => {
 				resolve({ top: 0, left: 0, behavior: "instant" });
-			}, 10);
+			});
 		});
 	},
-} as RouterConfig;
+} satisfies RouterConfig;
